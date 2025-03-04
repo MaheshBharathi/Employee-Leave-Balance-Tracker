6f3918ebe767231ef895580eb083d123b36b540a
MaheshBharathi <maheshbharathi2005@gmail.com>
2025-03-04 14:44:01 +0530

    Update Leaveinfo.js

--- frontend/src/Components/Leaveinfo.js
+++ frontend/src/Components/Leaveinfo.js
@@ -1,40 +1,4 @@
-// import React from 'react'
-// import "./Leaveinfo.css";
-// function Leaveinfo() {
-//   return (
-//     <div>
-//     <div className='l1'>
-//     <h1>Reason for leave</h1>
-//     <input className='c111'type='text' placeholder='Enter Your Reason'></input>
-//     <h1>How Many Days Will You Need</h1>
-//     <input className='c222'type='text' placeholder='Enter a Days'></input>
-//     <textarea></textarea>
-//     <button>SUBMIT</button>
-// </div>
-// </div>
-//   )
-// }
-// export default Leaveinfo
-//mine
-// import React from 'react'
-// import "./Leaveinfo.css"
-// function Leaveinfo() {
-//   return (
-//     <div>
-//       <div className='ll1'>
-//       <div className='l1'>
-//    <h1>Reason for leave</h1>
-//     <input className='c111'type='text' placeholder='Enter Your Reason'></input>
-//     <h1 className='re1'>How Many Days Will You Need</h1>
-//     <input className='c222'type='text' placeholder='Enter a Days'></input>
-//     <textarea className='re2'></textarea>
-//          <button className='re3'>SUBMIT</button>
-// </div>
-// </div>
-//     </div>
-//   )
-// }
-// export default Leaveinfo
+
 import React, { useState } from 'react';
 import "./Leaveinfo.css";
 
