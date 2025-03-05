1d45f207eee71cbaeb6f52551ab7181b7a0adf9a
MaheshBharathi <maheshbharathi2005@gmail.com>
2025-03-05 09:35:29 +0530

    Update ReqAuth.js

--- frontend/src/Components/ReqAuth.js
+++ frontend/src/Components/ReqAuth.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import { useAuth } from './Auth'
-import { Navigate, useNavigate } from 'react-router-dom'
+//import React from 'react'
+//import { useAuth } from './Auth'
+//import { Navigate, useNavigate } from 'react-router-dom'
 
-export default function ReqAuth(props) {
-    const auth=useAuth()
-    if(!auth.user){
-        return <Navigate to='/login'/>
-    }
-  return props.children
-}
+//export default function ReqAuth(props) {
+   // const auth=useAuth()
+    //if(!auth.user){
+        //return <Navigate to='/login'/>
+   // }
+ // return props.children
+//}
